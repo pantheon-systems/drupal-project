b564849e8976763d40f1416d34dcc5162452b818
Kevin Porras <kporras07@gmail.com>
2021-11-01 18:01:46 -0600

    Fix automation to keep separate git histories.

--- devops/scripts/deploy-public-upstream.sh
+++ devops/scripts/deploy-public-upstream.sh
@@ -56,15 +56,17 @@ done
 
 git commit -F /tmp/commit_message --author='Pantheon Automation <bot@getpantheon.com>'
 
-# update the release-pointer
-git tag -f -m 'Last commit set on upstream repo' release-pointer HEAD
-
 # Push released commits to a few branches on the upstream repo.
 # Since all commits to this repo are automated, it shouldn't hurt to put them on both branch names.
 release_branches=('master' 'main')
 for branch in "${release_branches[@]}"; do
   git push public public:"$branch"
 done
+
+git checkout $CIRCLE_BRANCH
+
+# update the release-pointer
+git tag -f -m 'Last commit set on upstream repo' release-pointer HEAD
 
 # Push release-pointer
 git push -f origin release-pointer
